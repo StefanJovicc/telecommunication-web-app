f7cb35fa51818c77ae43926e3cc8cc23d6705bee
Stefan Jović <stefanjovic562@yahoo.com>
2024-08-05 04:10:34 +0200

    Initial commit - Added Domain project layer

---
+++ TelecommunicationWebApp.Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelecommunicationWebApp.Domain
+{
+    public abstract class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
